ed9c85acd1f4b84e4cac866940dc297a9897b6b9
wgetgoose <louiscarsonferreira@gmail.com>
2022-05-11 21:05:38 -0700

    Changes to dracula with git status and profile loading with dotfiles

---
+++ user-profile.ps1
@@ -0,0 +1,23 @@
+oh-my-posh init pwsh --config C:\Users\louis\coding\dotfiles\custom-dracula.json | Invoke-Expression
+
+Import-Module PSReadLine
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -EditMode Emacs
+
+# Import-Module posh-git
+# Not using posh-git currently, instead using oh-my-posh's git segment
+Import-Module Terminal-Icons
+
+function upall {
+    scoop update *
+    winget upgrade --all
+}
+
+function vscodium {
+    param([string]$path)
+    Start-Process -WindowStyle hidden -FilePath "codium" -ArgumentList "$path"
+}
+
+Set-Alias grep findstr
+Set-Alias ll ls
