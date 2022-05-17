304105f876294c5d00f5311246b87fc1cda4e57d
wgetgoose <louiscarsonferreira@gmail.com>
2022-05-17 14:59:39 -0700

    Tab completion for winget-cli

--- windows/user-profile.ps1
+++ windows/user-profile.ps1
@@ -5,8 +5,9 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -EditMode Emacs
 
-# Import-Module posh-git
 # Not using posh-git currently, instead using oh-my-posh's git segment
+# Import-Module posh-git
+
 Import-Module Terminal-Icons
 
 function upall {
@@ -17,6 +18,17 @@ function upall {
 Set-Alias grep findstr
 Set-Alias ll ls
 Set-Alias vscodium codium.cmd
+
+# Tab-Complete / Autocomplete for winget: https://github.com/microsoft/winget-cli/blob/master/doc/Completion.md
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
 
 # Fixes launch bug with unexpected spaces, see here => https://github.com/JanDeDobbeleer/oh-my-posh/issues/1029
 clear
